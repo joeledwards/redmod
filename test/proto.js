138773d2d0451572d3e5435d17271da4f910a61f
Joel Edwards <joeledwards@gmail.com>
2018-03-14 00:01:41 -0600

    fix: corrected array structure and added test for nested arrays

--- test/proto.js
+++ test/proto.js
@@ -43,7 +43,15 @@ tap.test('proto.array()', t => {
   t.equal(array([null]).toString(), '*1\r\n$-1\r\n')
   t.equal(array([13]).toString(), '*1\r\n:13\r\n')
   t.equal(array([1, 2]).toString(), '*2\r\n:1\r\n:2\r\n')
+  t.equal(array(['foo']).toString(), '*1\r\n$3\r\nfoo\r\n')
   t.equal(array(['foo', 'bar']).toString(), '*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n')
-  t.equal(array([42, 'the answer']).toString(), '*2\r\n:42\r\n$10\r\nthe answer\r\n')
+  t.equal(
+    array([42, 'the answer']).toString(),
+    '*2\r\n:42\r\n$10\r\nthe answer\r\n'
+  )
+  t.equal(
+    array([['a', 'b'], [1, 2, 3]]).toString(),
+    '*2\r\n*2\r\n$1\r\na\r\n$1\r\nb\r\n*3\r\n:1\r\n:2\r\n:3\r\n'
+  )
   t.done()
 })
